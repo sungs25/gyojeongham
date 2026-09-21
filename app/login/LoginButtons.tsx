ee1888e635ad738ddd2247fb4b7e48236293e413
Sungmin <lsm0205l56@gmail.com>
2026-09-21 16:17:40 +0900

    4-4: 카카오 로그인

--- app/login/LoginButtons.tsx
+++ app/login/LoginButtons.tsx
@@ -3,7 +3,7 @@
 import { createClient } from '@/lib/supabase/client';
 
 export function LoginButtons() {
-  async function signIn(provider: 'google') {
+  async function signIn(provider: 'google' | 'kakao') {
     const supabase = createClient();
     await supabase.auth.signInWithOAuth({
       provider,
@@ -14,8 +14,13 @@ export function LoginButtons() {
   }
 
   return (
-    <button type="button" onClick={() => signIn('google')}>
-      구글로 로그인
-    </button>
+    <>
+      <button type="button" onClick={() => signIn('google')}>
+        구글로 로그인
+      </button>
+      <button type="button" onClick={() => signIn('kakao')}>
+        카카오로 로그인
+      </button>
+    </>
   );
 }
